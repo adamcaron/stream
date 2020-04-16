61fab40bfdcf654f094876946142b4e1426c4a9c
Adam Caron <adamwcaron@gmail.com>
2020-04-16 16:13:46 -0400

    component template

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
-Vue.component('task', {
-  template: '<li><slot></slot></li>',
+Vue.component('task-list', {
+  template: `
+    <div>
+      <task v-for="task in tasks">
+        {{ task.description }}
+      </task>
+    </div>
+  `,
+
   data() {
-     return {
-       message: 'Hi there!'
-     }
-  },
+    return {
+      tasks: [
+        { description: 'Go to the store', complete: true },
+        { description: 'Go to the email', complete: false },
+        { description: 'Go to the farm', complete: true },
+        { description: 'Go to work', complete: false },
+      ]
+    }
+  }
 })
 
-Vue.component('task-button', {
-  template: '<button><slot></slot></button>',
+Vue.component('task', {
+  template: '<li><slot></slot></li>',
 })
 
 new Vue({

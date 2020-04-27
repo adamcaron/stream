2e25d4731dc6a0110ace663c89f282cb26fa6363
Adam Caron <adamwcaron@gmail.com>
2020-04-27 13:08:27 -0400

    import child component

--- stream/src/App.vue
+++ stream/src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="app">
+    <message>Welcome to Stream!</message>
+    <message>We're glad you're here.</message>
   </div>
 </template>
 
 <script>
+import Message from './components/Message.vue'
+
 export default {
   name: 'app',
+  components: { Message },
   data () {
     return {
     }
